a45ff16ee89b8e9d3c37e587953dd7a4bd003751
hungtmd <hunglq7@gmail.com>
2025-01-23 15:44:13 +0700

    hoàn thiện thongsokythuatquatgio

--- obj/Debug/net7.0-windows/WebApi.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb657207700ce0e7b1679379172ac1d7f54e9115")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b615b632ec68b0234304761c015fb67b833acd2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
