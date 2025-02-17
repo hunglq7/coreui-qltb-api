fe4e793c5ab434da9da614c1954b3ec433977480
Le Quang Hung <hunglq7@gmail.com>
2025-02-17 21:58:32 +0700

    Tonghoptoitruc-17/02/2025-22h

--- Services/TonghoptoitrucService.cs
+++ Services/TonghoptoitrucService.cs
@@ -14,20 +14,21 @@ namespace WebApi.Services
         Task<int> Update(TonghoptoitrucUpdateRequest request);
         Task<int> Delete(int id);
         Task<TongHopToiTruc> GetById(int id);
+
         Task<PagedResult<TonghoptoitrucVm>> GetAllPaging(GetManagerTonghoptoitrucPagingRequest request);
     }
-    public class TonghoptoitrucService:ITonghoptoitrucService
+    public class TonghoptoitrucService : ITonghoptoitrucService
     {
         private readonly ThietbiDbContext _thietbiDbContext;
         public TonghoptoitrucService(ThietbiDbContext thietbiDbContext)
         {
-            _thietbiDbContext=thietbiDbContext;
-            
+            _thietbiDbContext = thietbiDbContext;
+
         }
 
         public async Task<int> Create(TonghoptoitrucCreateRequest request)
         {
-            if(request == null)
+            if (request == null)
             {
                 return 0;
             }
@@ -35,14 +36,14 @@ namespace WebApi.Services
             {
                 Id = request.Id,
                 MaQuanLy = request.MaQuanLy,
-                ThietbiId= request.ThietbiId,
-                DonViSuDungId= request.DonViSuDungId,
-                ViTriLapDat= request.ViTriLapDat,
-                NgayLap= request.NgayLap,
-                MucDichSuDung= request.MucDichSuDung,
-                SoLuong= request.SoLuong,
-                TinhTrangThietBi=request.TinhTrangThietBi,
-                GhiChu=request.GhiChu
+                ThietbiId = request.ThietbiId,
+                DonViSuDungId = request.DonViSuDungId,
+                ViTriLapDat = request.ViTriLapDat,
+                NgayLap = request.NgayLap,
+                MucDichSuDung = request.MucDichSuDung,
+                SoLuong = request.SoLuong,
+                TinhTrangThietBi = request.TinhTrangThietBi,
+                GhiChu = request.GhiChu
 
             };
             await _thietbiDbContext.AddRangeAsync(items);
@@ -61,28 +62,30 @@ namespace WebApi.Services
 
         }
 
+
         public async Task<PagedResult<TonghoptoitrucVm>> GetAllPaging(GetManagerTonghoptoitrucPagingRequest request)
         {
             var query = from t in _thietbiDbContext.TongHopToiTrucs.Include(x => x.ToiTruc).Include(x => x.PhongBan)
                         select t;
             if (!string.IsNullOrEmpty(request.Keyword))
             {
-                query=query.Where(x=>x.MaQuanLy.Contains(request.Keyword));
+                query = query.Where(x => x.MaQuanLy.Contains(request.Keyword));
             }
             int totalRow = await query.CountAsync();
+
             var data = await query.Skip((request.PageIndex - 1) * request.PageSize)
                 .Take(request.PageSize)
                 .Select(x => new TonghoptoitrucVm()
                 {
                     Id = x.Id,
-                    MaQuanLy=x.MaQuanLy,
-                   TenThietBi=x.ToiTruc.MaHieu,
+                    MaQuanLy = x.MaQuanLy,
+                    TenThietBi = x.ToiTruc.MaHieu,
                     PhongBan = x.PhongBan.TenPhong,
                     ViTriLapDat = x.ViTriLapDat,
                     NgayLap = x.NgayLap,
                     MucDichSuDung = x.MucDichSuDung,
                     SoLuong = x.SoLuong,
-                    TinhTrangThietBi = x.TinhTrangThietBi,      
+                    TinhTrangThietBi = x.TinhTrangThietBi,
 
                 }).ToListAsync();
             var pagedResult = new PagedResult<TonghoptoitrucVm>()
@@ -98,15 +101,15 @@ namespace WebApi.Services
         public async Task<TongHopToiTruc> GetById(int id)
         {
             var query = await _thietbiDbContext.TongHopToiTrucs.FindAsync(id);
-            if(query== null)
+            if (query == null)
             {
                 query = new TongHopToiTruc()
                 {
                     ThietbiId = 0,
-                    DonViSuDungId=0,
-                    NgayLap=new DateTime()
+                    DonViSuDungId = 0,
+                    NgayLap = new DateTime()
                 };
-               
+
             }
             return query;
         }
@@ -120,13 +123,13 @@ namespace WebApi.Services
             }
             query.MaQuanLy = request.MaQuanLy;
             query.ThietbiId = request.ThietbiId;
-            query.DonViSuDungId= request.DonViSuDungId;
+            query.DonViSuDungId = request.DonViSuDungId;
             query.ViTriLapDat = request.ViTriLapDat;
-            query.NgayLap= request.NgayLap;
+            query.NgayLap = request.NgayLap;
             query.MucDichSuDung = request.MucDichSuDung;
-            query.SoLuong=request.SoLuong;
+            query.SoLuong = request.SoLuong;
             query.TinhTrangThietBi = request.TinhTrangThietBi;
-            query.GhiChu=request.GhiChu;
+            query.GhiChu = request.GhiChu;
             return await _thietbiDbContext.SaveChangesAsync();
         }
 
